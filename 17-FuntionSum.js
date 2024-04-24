8400a987cb669a36963feb5df954317782e09467
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-24 17:32:36 +0530

    Sum using Funtion

---
+++ 17-FuntionSum.js
@@ -0,0 +1,10 @@
+//In this through function we can do many task with single line of code  
+
+function Sum(a,b){
+    console.log(`Sum of ${a} and ${b} = ${a+b}`)
+}
+
+Sum(10,20)
+Sum(140,20)
+Sum(1880,20)
+Sum(1155650,20)
