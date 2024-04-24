77ff7eb21619403730d9c38230ee96117bdb33b5
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-24 17:28:06 +0530

    destructuring

---
+++ 12.destructuring.js
@@ -0,0 +1,18 @@
+const Laptop1 ={
+    BrandName:'Asus',
+    RAM:'8gb',
+    SSD:'265gb',
+    processor:'ryzen 7'
+}
+const Laptop2 ={
+    BrandName:'HP',
+    RAM:'8gb',
+    SSD:'265gb',
+    processor:'ryzen 7'
+}
+
+const {BrandName,RAM ,SSD,processor} = Laptop1 //object destructuring 
+
+console.log(BrandName,RAM,processor);
+
+
