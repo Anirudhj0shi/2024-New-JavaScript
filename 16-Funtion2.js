c9c4f3a2a03eaa2e483924a851d5aab1507f2bd3
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-24 17:31:56 +0530

    Funtion stating program 2

---
+++ 16-Funtion2.js
@@ -0,0 +1,15 @@
+let a= 10;
+let b = 20;
+console.log(`Sum of ${a} and ${b} = ${a+b}`);
+
+a= 100;
+b = 200;
+console.log(`Sum of ${a} and ${b} = ${a+b}`);
+
+a= 100551;
+b = 20;
+console.log(`Sum of ${a} and ${b} = ${a+b}`);
+
+ a= 14580;
+b = 20;
+console.log(`Sum of ${a} and ${b} = ${a+b}`);
