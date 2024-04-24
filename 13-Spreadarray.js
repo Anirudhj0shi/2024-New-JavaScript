847e9c85a24668e5bb8942a2433e61681cd7183b
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-24 17:29:27 +0530

    Spread Array

---
+++ 13-Spreadarray.js
@@ -0,0 +1,16 @@
+const number1 = [10,20,30,40];
+
+const number2 = [50,60,70];
+
+console.log(number2)
+
+
+
+//used spread operator to copy number 4 object in 5
+const number4 = [10,20,30,40];
+
+const number5 = [...number4,50,60,70];
+
+console.log(number5)
+
+
