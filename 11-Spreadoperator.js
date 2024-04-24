b207960e7dceecf100058f0cb6ca67823f8fa01d
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-24 17:27:35 +0530

    Spread Operator

---
+++ 11-Spreadoperator.js
@@ -0,0 +1,10 @@
+const Laptop1 ={
+    BrandName:'Asus',
+    RAM:'8gb',
+    SSD:'265gb',
+    processor:'ryzen 7'
+}
+const Laptop2 ={...Laptop1, BrandName:'HP',RAM:'24gb'};
+
+console.log(Laptop1)
+console.log(Laptop2)
