816fe02cb0ea70291e2e07d42d58908724d17ba2
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-25 17:33:29 +0530

    Calculator wit arrow function

---
+++ 22-CalculatorArrowFun.js
@@ -0,0 +1,21 @@
+// const sum = (a,b) => a+b
+// console.log(sum(10,20))
+
+// const mul = (a,b) => a*b
+// console.log(mul(10,20))
+
+// const sub = (a,b) => a-b
+// console.log(sub(10,20))
+
+// const div = (a,b) => a/b
+// console.log(div(10,20))
+
+
+const SUM = (a,b) => 
+{
+    console.log(`division of ${a} and ${b} = ${a+b}`) 
+    return a+b;
+} 
+SUM(10,500)
+
+
