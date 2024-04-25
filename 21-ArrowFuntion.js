168374cfa4fa953296022ed0643dd133c9a0692c
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-25 17:32:42 +0530

    Arrow Funtion

---
+++ 21-ArrowFuntion.js
@@ -0,0 +1,13 @@
+// function print(){
+//     console.log("This is a normal function")
+// }
+// print()
+
+//this is an example of arrow Function 
+
+// const print = () => console.log("THis is arrow Function ")
+
+// print()
+
+const sum = (a,b) => a+b
+console.log(sum(10,20))
