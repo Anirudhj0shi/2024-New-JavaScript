da1db24b2fd48d63c7cc8e8b9314a29740741a19
Anirudh joshi <yashjoshi5521@gmail.com>
2024-04-26 17:33:23 +0530

    For loop in JS

---
+++ 27-For-in-loop.js
@@ -0,0 +1,13 @@
+//this is how u ittrate object
+const Marvel ={
+    moviename:'Infinity war',
+    release:2017,
+    hero:'Ironman',
+    Rating:'4 Stars',
+}
+
+for (const key in Marvel) {
+    // console.log(`${key} : ${Marvel[key]}`);
+    // console.log(`${key} : ${Marvel[key]}`);
+    console.log(`${key}`);
+}
