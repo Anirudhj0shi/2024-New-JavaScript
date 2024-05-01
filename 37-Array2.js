50686b94a3c8e67f0d3974f220dc4a829c161e8f
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-01 17:40:32 +0530

    Array Funtions 2

---
+++ 37-Array2.js
@@ -0,0 +1,9 @@
+const arr = [10,20,30,40,"Superman","Spiderman",30.45,false] 
+//before
+console.log(arr)
+
+//push => insert new element in the ending of the array
+
+//after
+arr.push("batman")
+console.log(arr)
