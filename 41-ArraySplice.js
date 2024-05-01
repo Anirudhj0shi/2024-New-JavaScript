c2eff72c49212ee87e0ec80ce92c41edd2d63e92
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-01 18:56:40 +0530

    Array predefined function Splice()

---
+++ 41-ArraySplice.js
@@ -0,0 +1,7 @@
+const arr = [10,20,30,40,"Superman","Spiderman",30.45,false] 
+//before
+console.log(arr);
+//Splice => this removes the value from the given index like (4(index) , 2(how many elements u want to remove))
+arr.splice(4,2)
+//after Modification
+console.log(arr);
