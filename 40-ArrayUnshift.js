362463e03f460b40e2f42e6c14a38e7cb67990cc
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-01 18:55:39 +0530

    Array predefined function Unshift()

---
+++ 40-ArrayUnshift.js
@@ -0,0 +1,9 @@
+const arr = [10,20,30,40,"Superman","Spiderman",30.45,false] 
+//before
+console.log(arr)
+
+//UnShift => add a new element in the beginning of the array
+
+//after
+arr.unshift("batman",2,3)
+console.log(arr)
