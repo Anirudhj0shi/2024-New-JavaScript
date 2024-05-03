3a72992d67ce33e1edcfca7906223ba95d7e6813
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-03 15:16:05 +0530

    Searching methods like indexOf using array in JS

---
+++ 47-ArraySearchingMethod.js
@@ -0,0 +1,11 @@
+//indexOf  => This is one of Array searching method
+
+
+const Number = ["Anirudh","Yash","Babli","..123","Sanju","Amit"]
+console.log(Number.indexOf("Babli"))
+
+
+//lastindexOf  => This is one of Array searching method
+
+const Number1 = ["Anirudh","Yash","Yash","Yash","Babli","..123","Sanju","Amit"]
+console.log(Number1.lastIndexOf("Babli"))
