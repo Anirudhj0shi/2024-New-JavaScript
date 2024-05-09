090202434dd63abe9369731203f8ceb28a0fed50
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-09 17:25:06 +0530

    taking character out in js

---
+++ 53-Taking-everychar-out-ofJS.js
@@ -0,0 +1,10 @@
+//charAt - to find a character in particular index
+const srt  = "Superman"
+
+//to find length of an array
+console.log("To find the length of array =>",srt.length)
+
+// console.log(srt.charAt(0))
+for(let i = 0; i<srt.length ; i++){
+    console.log(srt.charAt(i))
+}
