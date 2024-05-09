ffe75c609b11d773f46d344ae13a6acc1c23a63f
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-09 17:24:30 +0530

    Character in js

---
+++ 52-Characters-inJS.js
@@ -0,0 +1,7 @@
+//charAt - to find a character in particular index
+const srt  = "Superman"
+
+//to find length of an array
+console.log("To find the length of array =>",srt.length)
+
+console.log(srt.charAt(0))
