be54aecb57976e4cfbf108f88367a0dc83b9eb95
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-09 17:27:32 +0530

    String slice() function

---
+++ 58-StringSlice.js
@@ -0,0 +1,10 @@
+const string = "Anirudh"
+//it will include the first(Index) one but not include the last index number
+// A n i r u d h 
+// 0 1 2 3 4 5 6 
+
+
+console.log(string.slice(1,5))
+
+//n i r u 
+//1 2 3 4
