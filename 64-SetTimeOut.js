d8f1bb7c5972a5866b7bde426d2a916f06b114da
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-13 16:24:09 +0530

    Set Timeout

---
+++ 64-SetTimeOut.js
@@ -0,0 +1,9 @@
+// console.log("setTimeout")
+// setTimeout(()=>{
+//     console.log("inside set Timeout")
+// },2000)
+
+console.log("setInterval ")
+setInterval(() => {
+    console.log("Jai ho")
+}, 1000);// this is an infinite loop 
